01ba61152f3e28dc8105b3b5f561cf752859cbcb
Anatoly Politsin <politsin@gmail.com>
2016-09-01 11:55:34 +0300

    ready

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-cd /opt/docker-mailhog
-docker-compose up -d docker-mailhog
+cd /opt/docker-exim
+docker-compose up -d docker-exim
+
+#openssl req -x509 -nodes -days 9000 -newkey rsa:2048 -keyout /opt/docker-exim/ssl/exim.key -out /opt/docker-exim/ssl/exim.cert
+
+#docker run --rm \
+#  -p 25:25 \
+#  -v /opt/docker-exim/config/exim-smtp.conf:/etc/exim/exim.conf:ro \
+#  -v /opt/docker-exim/log/:/var/log/exim/ \
+#  -v /opt/docker-exim/ssl/:/etc/ssl/ \
+#  docker-exim 
