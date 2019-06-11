b66e662e754c3734cd1320d5f8ac47fe93e317fa
Anatoly Politsin <politsin@gmail.com>
2019-06-11 11:31:36 +0300

    update

--- build.py
+++ build.py
@@ -3,5 +3,5 @@
 import os
 if os.path.exists('/opt/docker-exim'):
   os.chdir('/opt/docker-exim')
-  os.system('docker build -t exim:4.91-r2 .')
-  os.system('docker tag exim:4.91-r2 exim:latest')
+  os.system('docker build -t exim:4.92 .')
+  os.system('docker tag exim:4.92 exim:latest')
